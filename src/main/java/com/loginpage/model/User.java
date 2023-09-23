047cf9f3aad2d49b46e355dc2eee4ab305cfb248
byzxc <darrenlinby@hotmail.com>
2023-09-23 18:01:23 +0800

    [feat] Added Repository for User

--- src/main/java/com/loginpage/model/User.java
+++ src/main/java/com/loginpage/model/User.java
@@ -1,5 +1,6 @@
 package com.loginpage.model;
 
+import com.loginpage.repository.Role;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,13 +18,19 @@ public class User {
     private String username;
     // Change to Encoded version later
     private String password;
+    private Role role;
 
-    public User(final String name, final String password) {
+    public User(final String name, final String password, final Role role) {
         this.username = name;
         this.password = password;
+        this.role = role;
     }
 
-    public String getUsername() {
+    public final String getUsername() {
         return username;
+    }
+
+    public final Role getRole() {
+        return role;
     }
 }
