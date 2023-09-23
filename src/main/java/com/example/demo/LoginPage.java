10ecbc4dabfd28d4f10478313dc2270ec5167d39
byzxc <darrenlinby@hotmail.com>
2023-09-23 16:12:19 +0800

    [feat] Add springBoot and gradle tool automation

---
+++ src/main/java/com/example/demo/LoginPage.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPage {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginPage.class, args);
+    }
+
+}
