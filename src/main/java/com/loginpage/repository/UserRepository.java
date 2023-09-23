b77f942e5b2eee3ac34b157faf060c6ef46407e9
byzxc <darrenlinby@hotmail.com>
2023-09-23 18:01:23 +0800

    [feat] Added Repository for User

--- src/main/java/com/loginpage/repository/UserRepository.java
+++ src/main/java/com/loginpage/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.loginpage.repository;
 
 import com.loginpage.model.User;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
@@ -8,8 +9,13 @@ import java.util.Optional;
 import java.util.Set;
 
 @Repository
+@NoArgsConstructor
 public class UserRepository {
-    private final Set<User> users = new HashSet<>();
+    private Set<User> users = new HashSet<>();
+
+    public UserRepository(final HashSet<User> users) {
+        this.users = users;
+    }
 
     public Optional<User> findByName(String username) {
         return users.stream()
@@ -17,11 +23,11 @@ public class UserRepository {
                 .findFirst();
     }
 
-    public void save(User user) {
-        users.add(user);
+    public void save(final Set<User> users) {
+        this.users.addAll(users);
     }
 
-    public void delete(User user) {
-        users.remove(user);
+    public void delete(final Set<User> users) {
+        this.users.removeAll(users);
     }
 }
--- src/test/java/com/loginpage/repository/UserRepositoryTest.java
+++ src/test/java/com/loginpage/repository/UserRepositoryTest.java
@@ -4,7 +4,9 @@ import com.loginpage.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,32 +16,32 @@ public class UserRepositoryTest {
     @BeforeEach
     public void setUp() {
         // Initialize the in-memory repository
-        userRepository = new UserRepository();
+        userRepository = new UserRepository(new HashSet<>());
     }
 
     @Test
     public void testSaveAndFindByName() {
-        User user = new User("testuser", "password");
-        userRepository.save(user);
+        final User user = new User("testuser", "password", Role.NormalUser);
+        userRepository.save(Set.of(user));
 
-        Optional<User> foundUser = userRepository.findByName("testuser");
+        final Optional<User> foundUser = userRepository.findByName("testuser");
         assertTrue(foundUser.isPresent());
         assertEquals("testuser", foundUser.get().getUsername());
     }
 
     @Test
     public void testFindByNameNotFound() {
-        Optional<User> foundUser = userRepository.findByName("nonexistentuser");
+        final Optional<User> foundUser = userRepository.findByName("nonexistentuser");
         assertFalse(foundUser.isPresent());
     }
 
     @Test
     public void testDelete() {
-        User user = new User("testuser", "password");
-        userRepository.save(user);
+        final User manager = new User("testmgr", "password", Role.Manager);
+        userRepository.save(Set.of(manager));
 
-        userRepository.delete(user);
-        Optional<User> foundUser = userRepository.findByName("testuser");
+        userRepository.delete(Set.of(manager));
+        Optional<User> foundUser = userRepository.findByName("testmgr");
         assertFalse(foundUser.isPresent());
     }
 }
